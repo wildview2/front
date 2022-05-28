8e4be8104b48a8920febc03e7e0f754c8de03b0d
Алексей Кругликов <akrug555@yandex.ru>
2022-05-28 06:43:16 +0300

    new changes

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Background from "./../imgs/bg.jpg"
 import styled from "styled-components"
-
+import Calendarr from "../components/Calendar"
 import { theme } from "./../../theme"
 
 const Wrapper = styled.div`
@@ -56,10 +56,53 @@ const Submit = styled.button`
   height: auto;
   opacity: 0.62;
 `
+const Main = styled.div`
+  display: flex;
 
+`
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const Right = styled.div`
+  display: flex;
+
+`
+const Center = styled.div`
+  display: flex;
+
+`
+const Text = styled.span`
+color: black;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 89px;
+  padding: 17px 0px 17px 41px;
+`
+const Input = styled.input`
+  display: flex;
+  width: 100%;
+  background: white;
+  border: unset;
+  border-radius: 16px;
+  padding: 16px;
+  height: auto;
+`
+const Calendarik = styled.div`
+display: flex;
+  width: 100%;
+  background: white;
+  border: unset;
+  border-radius: 16px;
+  padding: 16px;
+  height: 100px;
+`
 
 export const Home = () => {
   const [state, setState] = React.useState(false)
+  const [legbishe, setLegbishe] = React.useState()
+  const [date, Calendarr] = React.useState()
 
   return (
     <Wrapper onClick={() => setState(!state)}>
@@ -68,6 +111,27 @@ export const Home = () => {
                 <Zag>Wildview</Zag>
                 <Submit>Выйти</Submit>
             </Head>
+            <Main>
+              <Left>
+                <Text>Выберите лежбище и дату</Text>
+                <Input
+                type='text'
+                value={legbishe}
+                onChange={e=>setLegbishe(e.target.value)}>
+                </Input>
+                <Calendarik
+                type
+                value={date}
+                onChange={e=>Calendarr(e.target.value)}>
+                </Calendarik>
+              </Left>
+              <Center>
+
+              </Center>
+              <Right>
+
+              </Right>
+            </Main>
             {state.toString()}
         </Content>
     </Wrapper>
